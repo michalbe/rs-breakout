9f18749af8b4ef5c8ebaf3048b37ce0a7843162e
Michał Budzyński <michal@virtualdesign.pl>
2019-07-13 21:31:00 +0100

    :train2: Transform::traslation

--- src/components/com_transform.rs
+++ src/components/com_transform.rs
@@ -1,9 +1,17 @@
-use crate::math::mat4::Mat4;
+use crate::math::{
+    mat4::Mat4,
+    vec3::Vec3,
+};
 
 #[derive(Clone, Copy)]
 pub struct Transform {
+    // Matrix relative to the world
     world: Mat4,
+    // World to self matrix
     self_mat: Mat4,
+
+    // translation relative to the parent
+    translation: Vec3,
 }
 
 impl Transform {
@@ -11,6 +19,7 @@ impl Transform {
         Transform {
             world: Mat4::new(),
             self_mat: Mat4::new(),
+            translation: Vec3::new(),
         }
     }
 }
