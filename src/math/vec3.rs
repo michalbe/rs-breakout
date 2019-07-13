757619ce472a21317decf0807fa062abe6531445
Michał Budzyński <michal@virtualdesign.pl>
2019-07-13 21:31:00 +0100

    :outbox_tray: Math::Vec3

---
+++ src/math/vec3.rs
@@ -0,0 +1,16 @@
+#[derive(Clone, Copy)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3 {
+    pub fn new() -> Vec3 {
+        Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+}
