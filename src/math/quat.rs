b2c83ab4a2cc8d8d0a0edfa21cb344cc82dcd052
Michał Budzyński <michal@virtualdesign.pl>
2019-07-13 21:39:00 +0100

    :telephone_receiver: floats in quat

--- src/math/quat.rs
+++ src/math/quat.rs
@@ -9,10 +9,10 @@ pub struct Quat {
 impl Quat {
     pub fn new() -> Quat {
         Quat {
-            x: 0,
-            y: 0,
-            z: 0,
-            w: 1,
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+            w: 1.0,
         }
     }
 }
