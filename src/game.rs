8b2f0134c7936f510b9ab083c15da5e8858724b3
Michał Budzyński <michal@virtualdesign.pl>
2019-07-13 17:11:14 +0100

    Game::world

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,13 @@
+const MAX_ENTITIES: usize = 10000;
 
 pub struct Game {
-
+    world: [i32; MAX_ENTITIES],
 }
 
 impl Game {
     pub fn new() -> Game {
-        Game {}
+        Game {
+            world: [0; MAX_ENTITIES]
+        }
     }
 }
