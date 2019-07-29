7bdb7f120432262838059ae8020a342f833016e6
Michał Budzyński <michal@virtualdesign.pl>
2019-07-29 10:20:00 +0100

    :tomato: make mixins dyn

--- src/blueprints/blu_common.rs
+++ src/blueprints/blu_common.rs
@@ -10,5 +10,5 @@ pub struct Blueprint {
     pub translation: Option<Vec3>,
     pub rotation: Option<Quat>,
     pub scale: Option<Vec3>,
-    pub using: Vec<Box<Fn(&mut Game, usize) -> ()>>,
+    pub using: Vec<Box<dyn Fn(&mut Game, usize) -> ()>>,
 }
