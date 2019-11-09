7f8ad079a86da82af4ded75a8849c2a7856235f0
Michał Budzyński <michal@virtualdesign.pl>
2019-11-09 17:39:00 +0100

    :icecream: move worlds code to the external world

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,14 @@ mod math;
 mod components;
 mod blueprints;
 mod systems;
+mod worlds;
 
-use crate::math::vec2::Vec2;
-use crate::components::com_draw2d::Draw2d;
-use crate::{
-    game::*,
-    blueprints::blu_common::*,
-};
+use crate::worlds::wor_main::world_main;
+use crate::game::Game;
 
 fn main() {
     let mut game = Game::new(640, 480);
-    let translation = Vec2::new(100.0, 200.0);
-    let rotation = 0.0;
-    let scale = Vec2::new(2.0, 2.0);
 
-    let mut blueprint_without_mixins = Blueprint {
-        translation: Some(Vec2::new(200.0, 300.0)),
-        rotation: Some(rotation),
-        scale: Some(scale),
-        using: vec![
-            Box::new(Draw2d::new(Some(100), Some(20), Some([0, 255, 0, 255])))
-        ],
-    };
-
-    let mut blueprint_with_mixins = Blueprint {
-        translation: Some(translation),
-        rotation: Some(rotation),
-        scale: Some(scale),
-        using: vec![
-            Box::new(Draw2d::new(Some(50), Some(50), Some([255, 0, 255, 255])))
-        ],
-    };
-
-    let entity_1 = game.add(&mut blueprint_without_mixins);
-    // let entity_2 = game.add(&mut blueprint_with_mixins);
-
+    world_main(&mut game);
     game.start();
 }
---
+++ src/worlds/wor_main.rs
@@ -0,0 +1,32 @@
+
+use crate::blueprints::blu_common::Blueprint;
+use crate::game::Game;
+use crate::math::vec2::Vec2;
+use crate::components::com_draw2d::Draw2d;
+
+pub fn world_main(game: &mut Game) {
+    let translation = Vec2::new(100.0, 200.0);
+    let rotation = 0.0;
+    let scale = Vec2::new(2.0, 2.0);
+
+    let mut blueprint_without_mixins = Blueprint {
+        translation: Some(Vec2::new(200.0, 300.0)),
+        rotation: Some(rotation),
+        scale: Some(scale),
+        using: vec![
+            Box::new(Draw2d::new(Some(100), Some(20), Some([0, 255, 0, 255])))
+        ],
+    };
+
+    let mut blueprint_with_mixins = Blueprint {
+        translation: Some(translation),
+        rotation: Some(rotation),
+        scale: Some(scale),
+        using: vec![
+            Box::new(Draw2d::new(Some(50), Some(50), Some([255, 0, 255, 255])))
+        ],
+    };
+
+    let entity_1 = game.add(&mut blueprint_without_mixins);
+    let entity_2 = game.add(&mut blueprint_with_mixins);
+}
