d40439f67892651d14ebcc745b5aeaa53e09cc8c
Michał Budzyński <michal@virtualdesign.pl>
2019-11-09 18:45:00 +0100

    :bulb: controll ball mixin

--- src/blueprints/blu_ball.rs
+++ src/blueprints/blu_ball.rs
@@ -3,6 +3,7 @@ use crate::game::Game;
 use crate::blueprints::blu_common::Blueprint;
 use crate::components::com_draw2d::Draw2d;
 use crate::components::com_move::Move;
+use crate::components::com_controll_ball::ControlBall;
 
 pub fn get_ball(game: &mut Game, x: f32, y: f32) -> Blueprint {
     Blueprint {
@@ -12,6 +13,7 @@ pub fn get_ball(game: &mut Game, x: f32, y: f32) -> Blueprint {
         using: vec![
             Box::new(Draw2d::new(Some(20), Some(20), Some([0, 255, 0, 255]))),
             Box::new(Move::new(Some(Vec2::new(1.0, 1.0)), Some(20.0))),
+            Box::new(ControlBall::new(None)),
         ],
     }
 }
--- src/components/com_controll_ball.rs
+++ src/components/com_controll_ball.rs
@@ -19,14 +19,24 @@ impl ControlBall {
         }
     }
 
-    pub fn new(direction: Option<Vec2>) -> impl Fn(&mut Game, usize) -> () {
+    pub fn new(angle: Option<f32>) -> impl Fn(&mut Game, usize) -> () {
         move |game: &mut Game, entity: usize| -> () {
             game.world[entity] |= Has::ControlBall as u32;
 
             game.control_ball[entity] = Some(ControlBall {
-                direction: match direction {
-                    Some(direction_value) => { direction_value },
-                    None => { Vec2::empty() }
+                direction: match angle {
+                    Some(angle) => {
+                        println!("NIE ELO");
+                        let x = angle.cos();
+                        let y = angle.sin();
+                        Vec2::new(x, y)
+                     },
+                    None => {
+                        let default_angle: f32 = std::f32::consts::PI/2.0;
+                        let x = default_angle.cos();
+                        let y = default_angle.sin();
+                        Vec2::new(x, y)
+                    }
                 },
             });
         }
