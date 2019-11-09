0e15cb10e5a055306a697a037290f35c31c9de5b
Michał Budzyński <michal@virtualdesign.pl>
2019-11-10 00:05:00 +0100

    :church: com_cotrol_paddle added to paddle blueprint

--- src/blueprints/blu_paddle.rs
+++ src/blueprints/blu_paddle.rs
@@ -3,6 +3,8 @@ use crate::blueprints::blu_common::Blueprint;
 use crate::math::vec2::Vec2;
 use crate::components::com_draw2d::Draw2d;
 use crate::components::com_collide::Collide;
+use crate::components::com_control_paddle::ControlPaddle;
+use crate::components::com_move::Move;
 
 pub fn get_paddle(game: &mut Game, x: f32, y: f32) -> Blueprint {
     Blueprint {
@@ -12,6 +14,8 @@ pub fn get_paddle(game: &mut Game, x: f32, y: f32) -> Blueprint {
         using: vec![
             Box::new(Draw2d::new(Some(100), Some(20), Some([255, 0, 0, 255]))),
             Box::new(Collide::new(Some(Vec2::new(100.0, 20.0)))),
+            Box::new(Move::new(None, Some(90.0))),
+            Box::new(ControlPaddle::new()),
         ],
     }
 }
