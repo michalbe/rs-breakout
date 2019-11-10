d58a9d596745a64c006085f903454f0509aeb71f
Michał Budzyński <michal@virtualdesign.pl>
2019-11-10 23:15:00 +0100

    :sunrise: add ccontrol block flag to block blueprint

--- src/blueprints/blu_block.rs
+++ src/blueprints/blu_block.rs
@@ -3,6 +3,7 @@ use crate::blueprints::blu_common::Blueprint;
 use crate::math::vec2::Vec2;
 use crate::components::com_draw2d::Draw2d;
 use crate::components::com_collide::Collide;
+use crate::components::com_control_block::ControlBlock;
 
 pub fn get_block(game: &mut Game, x: f32, y: f32, width: u32, height: u32) -> Blueprint {
     Blueprint {
@@ -12,6 +13,7 @@ pub fn get_block(game: &mut Game, x: f32, y: f32, width: u32, height: u32) -> Bl
         using: vec![
             Box::new(Draw2d::new(Some(width), Some(height), Some([255, 255, 0, 255]))),
             Box::new(Collide::new(Some(Vec2::new(width as f32, height as f32)))),
+            Box::new(ControlBlock::new()),
         ],
     }
 }
