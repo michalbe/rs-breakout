180da8ae275d9362ca4c8bbaf6d7f6e92664a972
Michał Budzyński <michal@virtualdesign.pl>
2019-11-10 23:46:00 +0100

    :couplekiss: block control component

--- src/components/com_control_block.rs
+++ src/components/com_control_block.rs
@@ -13,7 +13,7 @@ pub struct ControlBlock { }
 impl ControlBlock {
     pub fn new() -> impl Fn(&mut Game, usize) -> () {
         move |game: &mut Game, entity: usize| -> () {
-            game.world[entity] |= Has::ControlBall as u32;
+            game.world[entity] |= Has::ControlBlock as u32;
         }
     }
 }
