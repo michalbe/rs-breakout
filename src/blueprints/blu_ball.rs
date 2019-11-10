721c381c0201ec52068184a88727270b82dafb96
Michał Budzyński <michal@virtualdesign.pl>
2019-11-10 23:01:00 +0100

    :bust_in_silhouette: add collide component to blueprints

--- src/blueprints/blu_ball.rs
+++ src/blueprints/blu_ball.rs
@@ -4,6 +4,7 @@ use crate::blueprints::blu_common::Blueprint;
 use crate::components::com_draw2d::Draw2d;
 use crate::components::com_move::Move;
 use crate::components::com_controll_ball::ControlBall;
+use crate::components::com_collide::Collide;
 
 pub fn get_ball(game: &mut Game, x: f32, y: f32) -> Blueprint {
     Blueprint {
@@ -14,6 +15,7 @@ pub fn get_ball(game: &mut Game, x: f32, y: f32) -> Blueprint {
             Box::new(Draw2d::new(Some(20), Some(20), Some([0, 255, 0, 255]))),
             Box::new(Move::new(Some(Vec2::new(1.0, 1.0)), Some(60.0))),
             Box::new(ControlBall::new(None)),
+            Box::new(Collide::new(Some(Vec2::new(20.0, 20.0)))),
         ],
     }
 }
--- src/blueprints/blu_paddle.rs
+++ src/blueprints/blu_paddle.rs
@@ -2,6 +2,7 @@ use crate::game::Game;
 use crate::blueprints::blu_common::Blueprint;
 use crate::math::vec2::Vec2;
 use crate::components::com_draw2d::Draw2d;
+use crate::components::com_collide::Collide;
 
 pub fn get_paddle(game: &mut Game, x: f32, y: f32) -> Blueprint {
     Blueprint {
@@ -9,7 +10,8 @@ pub fn get_paddle(game: &mut Game, x: f32, y: f32) -> Blueprint {
         rotation: None,
         scale: None,
         using: vec![
-            Box::new(Draw2d::new(Some(100), Some(20), Some([255, 0, 0, 255])))
+            Box::new(Draw2d::new(Some(100), Some(20), Some([255, 0, 0, 255]))),
+            Box::new(Collide::new(Some(Vec2::new(100.0, 20.0)))),
         ],
     }
 }
