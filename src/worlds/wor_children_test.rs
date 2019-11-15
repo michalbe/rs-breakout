49060c499d8037f333006afd76c7ae379fbdc0fc
Michał Budzyński <michal@virtualdesign.pl>
2019-11-15 22:28:01 +0100

    :man_with_gua_pi_mao: example world

--- src/worlds/wor_children_test.rs
+++ src/worlds/wor_children_test.rs
@@ -4,20 +4,36 @@ use crate::blueprints::blu_paddle::get_paddle;
 use crate::blueprints::blu_block::get_block;
 use crate::blueprints::blu_ball::get_ball;
 use crate::game::Game;
+use crate::components::com_draw2d::Draw2d;
 
 pub fn world_children_test(game: &mut Game) {
     let mut camera = Blueprint {
         translation: None,
         rotation: None,
         scale: None,
-        using: vec![],
+        using: vec![
+            Box::new(crate::components::com_move::Move::new(Some(crate::math::vec2::Vec2::new(1.0, 1.0)), Some(30.0))),
+            Box::new(crate::components::com_controll_ball::ControlBall::new(None)),
+        ],
         children: None,
     };
 
-    let mut ball = get_ball((game.window_width/2) as f32, (game.window_height/2) as f32);
-    let mut ball2 = get_ball((game.window_width/2 + 30) as f32, (game.window_height/2 + 30) as f32);
+    let ball = get_ball((game.window_width/2) as f32, (game.window_height/2) as f32);
+    let ball2 = get_ball((game.window_width/2 + 30) as f32, (game.window_height/2 + 30) as f32);
 
-    let camera_children = Some(vec![ball, ball2]);
+    let unmovable = Blueprint {
+        translation: None,
+        rotation: None,
+        scale: None,
+        using: vec![
+            // Box::new(crate::components::com_move::Move::new(Some(crate::math::vec2::Vec2::new(1.0, 0.0)), Some(30.0))),
+            Box::new(Draw2d::new(Some(50), Some(50), Some([123, 055, 031, 255]))),
+        ],
+        children: None,
+    };
+
+
+    let camera_children = Some(vec![unmovable, ball, ball2]);
 
     camera.children = camera_children;
 
