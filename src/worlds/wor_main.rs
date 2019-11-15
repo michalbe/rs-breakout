90f6c78ee4d079b9d9d34fdc209be8944b1676ca
Michał Budzyński <michal@virtualdesign.pl>
2019-11-15 22:40:19 +0100

    :headphones: camera entity

--- src/worlds/wor_main.rs
+++ src/worlds/wor_main.rs
@@ -1,4 +1,6 @@
 
+use crate::game::MAX_CHILDREN;
+use crate::blueprints::blu_common::Blueprint;
 use crate::blueprints::blu_paddle::get_paddle;
 use crate::blueprints::blu_block::get_block;
 use crate::blueprints::blu_ball::get_ball;
@@ -14,18 +16,29 @@ pub fn world_main(game: &mut Game) {
     let starting_x = (game.window_width - grid_width)/2;
     let starting_y = 50;
 
-    let paddle = get_paddle((game.window_width / 2) as f32, (game.window_height - 30) as f32);
-    game.add(paddle);
+    let mut camera = Blueprint {
+        translation: None,
+        rotation: None,
+        scale: None,
+        using: vec![],
+        children: None,
+    };
 
-    let ball = get_ball((game.window_width/2) as f32, (game.window_height/2) as f32);
-    game.add(ball);
+    let mut game_elements = vec![
+        get_paddle((game.window_width / 2) as f32, (game.window_height - 30) as f32),
+        get_ball((game.window_width/2) as f32, (game.window_height/2) as f32)
+    ];
+
 
     for row in 0..row_count {
         let y = starting_y + row * (block_height + margin) + block_height / 2;
         for col in 0..column_count {
             let x = starting_x + col * (block_width + margin) + block_width / 2;
-            let block = get_block(x as f32, y as f32, block_width, block_height);
-            game.add(block);
+            game_elements.push(get_block(x as f32, y as f32, block_width, block_height));
         }
     }
+
+    camera.children = Some(game_elements);
+
+    game.add(camera);
 }
