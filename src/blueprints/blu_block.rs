d871cc02501fb276087dd8533cfcba4537ef3062
Michał Budzyński <michal@virtualdesign.pl>
2019-11-16 03:35:55 +0100

    :feelsgood: fade on blocks

--- src/blueprints/blu_block.rs
+++ src/blueprints/blu_block.rs
@@ -3,6 +3,7 @@ use crate::math::vec2::Vec2;
 use crate::components::com_draw2d::Draw2d;
 use crate::components::com_collide::Collide;
 use crate::components::com_control_block::ControlBlock;
+use crate::components::com_fade::Fade;
 
 pub fn get_block(x: f32, y: f32, width: u32, height: u32) -> Blueprint {
     Blueprint {
@@ -13,6 +14,7 @@ pub fn get_block(x: f32, y: f32, width: u32, height: u32) -> Blueprint {
             Box::new(Draw2d::new(Some(width), Some(height), Some([255, 255, 0, 255]))),
             Box::new(Collide::new(Some(Vec2::new(width as f32, height as f32)))),
             Box::new(ControlBlock::new()),
+            Box::new(Fade::new(Some(0.0))),
         ],
         children: None,
     }
--- src/systems/sys_control_block.rs
+++ src/systems/sys_control_block.rs
@@ -2,7 +2,7 @@ use crate::components::Has;
 use crate::game::MAX_ENTITIES;
 use crate::game::Game;
 
-const QUERY: u32 = Has::ControlBlock as u32 | Has::Collide as u32;
+const QUERY: u32 = Has::ControlBlock as u32 | Has::Collide as u32 | Has::Fade as u32;
 
 pub fn sys_control_block(game: &mut Game, _delta: f32) {
     for i in 0..MAX_ENTITIES {
@@ -16,12 +16,16 @@ pub fn sys_control_block(game: &mut Game, _delta: f32) {
 fn update(game: &mut Game, entity: usize) {
     if let (
         Some(collide),
+        Some(mut fade),
     ) = (
         game.collide[entity],
+        game.fade[entity],
     ) {
 
         if let Some(_) = collide.collision {
-            game.destroy(entity);
+            fade.step = 0.02;
+            game.fade[entity] = Some(fade);
+            // game.world[entity] &= Has::Collide as u32;
         }
     }
 }
